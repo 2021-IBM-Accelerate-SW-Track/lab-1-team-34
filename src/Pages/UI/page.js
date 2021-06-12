44e497e674ae1b0c80454dc19296b7ea4d4b0ea7
jtagaca <jtagaca@csub.edu>
2021-06-11 19:38:43 -0700

    save point

--- src/Pages/UI/page.js
+++ src/Pages/UI/page.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../../component/header";
+import Input from "../../component/Input";
 
 function page(props) {
   return (
     <div>
       <Header />
+      <Input />
     </div>
   );
 }
--- src/component/Input/input.js
+++ src/component/Input/input.js
@@ -1,12 +1,46 @@
-import React from "react";
-import { TextField } from "@material-ui/core";
+import { TextField, Button } from "@material-ui/core";
+import React, { useState } from "react";
 
+const initialTodo = [
+  {
+    todoname: "go to the gym",
+  },
+  {
+    todoname: "finish IBM",
+  },
+];
 function Input(props) {
+  const [input, setInput] = useState("");
+  const [list, setTodo] = React.useState(initialTodo);
+
+  function handleAdd() {
+    const newList = list.concat({ input });
+
+    setTodo(newList);
+  }
   return (
     <div>
       <form noValidate autoComplete="off">
-        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+        <TextField
+          onChange={(v) => {
+            setInput(v);
+          }}
+          // onEnter={() => {
+          //   list.push(input);
+          // }}
+          // id="outlined-basic"
+          label="Outlined"
+          variant="outlined"
+        />
       </form>
+      <Button onClick={handleAdd} variant="contained" color="primary">
+        Submit
+      </Button>
+
+      {list &&
+        list.map((item, index) => {
+          return <li key={index}>{item.todoname}</li>;
+        })}
     </div>
   );
 }
